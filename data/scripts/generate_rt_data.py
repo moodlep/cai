1ff4c891976b3fa559e3cf1008f0a171f563036b
Perusha Moodley <perusha.moodley@gmail.com>
2025-01-27 11:44:52 +0000

    Made red team data gen more generic and cleaned up a bit

--- data/scripts/generate_rt_data.py
+++ data/scripts/generate_rt_data.py
@@ -1,8 +1,6 @@
 import json
 import os
 from datetime import datetime
-from importlib.metadata import distributions
-from pathlib import Path
 
 import openai
 from openai import OpenAI
@@ -11,6 +9,19 @@ import json
 from data_utils import get_principles_from_constitution, generate_formatted_response, get_config, GenPrompts
 from structured_response_formats import RTResponse, RedTeamPrompts
 import dotenv
+import argparse
+
+# add args
+parser = argparse.ArgumentParser(description='Generate red team prompts')
+parser.add_argument('--config', type=str, default='data/config/sa_config.yaml', help='path to the config file')
+parser.add_argument('--config-category', type=str, default='red_team', help='type of data generation: red_team, sft, dpo')
+parser.add_argument('--model', type=str, default='gpt-4o-mini', help='model to use for generation: gpt-4o-mini, gpt-4o')
+parser.add_argument('--verbose', type=str, default=True, help='verbosity setting for openAI model: True/False')
+parser.add_argument('--output-file', type=str, default="data/datasets/red_team_prompts", help='output file name')
+parser.add_argument('--include-few-shot', type=str, default=False, help='include few shot examples')
+
+args = parser.parse_args()
+
 
 # Configure your OpenAI API key
 dotenv.load_dotenv()
@@ -20,12 +31,11 @@ openai.api_key = api_key
 client = OpenAI()
 
 # Load config
-config = get_config('data/config/sa_config.yaml')
-rt_config = config['red_team']
-sft_config = config['sft']
+config = get_config(args.config)
+rt_config = config[args.config_category]
 
 # Load principles
-sac_principles = get_principles_from_constitution(rt_config['sa_principles'])
+principles = get_principles_from_constitution(rt_config['principles'])
 
 # Run to see the questions that the model generated
 gen_prompts = GenPrompts(system_prompt=rt_config['RT_SYSTEM_PROMPT'], user_prompt=rt_config['RT_USER_PROMPT'])
@@ -34,19 +44,19 @@ gen_prompts = GenPrompts(system_prompt=rt_config['RT_SYSTEM_PROMPT'], user_promp
 
 results = {}
 
-for id, principle in enumerate(sac_principles):
+for id, principle in enumerate(principles):
   print(f"Generating red-teaming prompts for principle id {id}-{principle}: ")
   
   messages = gen_prompts.get_message(principle=principle, num_to_gen=rt_config['RT_NUM_RTP_PER_CALL'])
   
-  response = generate_formatted_response(client=client, model=config['model'], messages=messages, verbose=True, response_format=RedTeamPrompts)
+  response = generate_formatted_response(client=client, model=args.model, messages=messages, verbose=args.verbose, response_format=RedTeamPrompts)
   print("MODEL RESPONSES:\n")
   results[id] = json.loads(response)["rt_responses"]
   print(results[id])
 
 # save results to json file
 timestr = datetime.now().strftime("%Y%m%d-%H%M%S")
-with open(f"data/datasets/red_team_prompts_{timestr}.json", 'w') as f:
+with open(f"{args.output_file}_{timestr}.json", 'w') as f:
     json.dump(results, f)
 
 
